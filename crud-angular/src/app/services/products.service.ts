c488483dad9adeb333fa672b68541bfc625c9f43
Josen3D <77815968+Josen3D@users.noreply.github.com>
2023-04-04 13:25:56 -0600

    add CRUD methods in products.service.ts

--- crud-angular/src/app/services/products.service.ts
+++ crud-angular/src/app/services/products.service.ts
@@ -1,17 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Producto } from '../shared/products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  URL = 'http://localhost:3000/api';
 
-URL = 'http://localhost:3000/api';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Get products method
-  getProducts(){
+  getProducts() {
     return this.http.get(this.URL + '/products');
+  }
+
+  getProduct(id: string) {
+    return this.http.get(this.URL + '/products/' + id);
+  }
+
+  //Create a product
+  createProduct(producto: Producto) {
+    return this.http.post(this.URL + '/products', producto);
+  }
+
+  //Update a product
+  updateProduct(id: string, producto: Producto) {
+    return this.http.patch(this.URL + '/products/' + id, producto);
+  }
+
+  //Delete a product
+  deleteProduct(id: string) {
+    return this.http.delete(this.URL + '/products/' + id);
   }
 }
