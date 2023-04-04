e1e83519d7c0f423ec6ddba25016e0e6cf779be1
Josen3D <77815968+Josen3D@users.noreply.github.com>
2023-04-04 11:15:24 -0600

    fix: add export for products.routes

--- api-nodejs/src/routes/products.routes.js
+++ api-nodejs/src/routes/products.routes.js
@@ -1,10 +1,14 @@
 //Import router from express
 import {Router} from "express";
 //Import products controller
-import { getProducts } from "../controllers/products.controller";
+import { getProducts } from "../controllers/products.controller.js";
 
 //Create the router
 const router = Router();
 
 //Create http methods connection
 router.get("/products", getProducts); 
+
+
+//Export the router
+export default router;
