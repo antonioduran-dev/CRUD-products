725461ce16d8ccf8ea04f68ba4f74aba2233627e
Josen3D <77815968+Josen3D@users.noreply.github.com>
2023-04-04 11:23:37 -0600

    add method and route for getProduct

--- api-nodejs/src/controllers/products.controller.js
+++ api-nodejs/src/controllers/products.controller.js
@@ -1,15 +1,34 @@
 //Import DB connection
-import {pool} from "../database/db.js";
-
+import { pool } from "../database/db.js";
 
 //Get all products from DB
 export const getProducts = async (req, res) => {
-    try {
-        const [rows] = await pool.query("SELECT * FROM products");
-        res.json(rows);
-    } catch (error) {
-        return res.status(500).json({
-            message: "Something went wrong"
-        });
-    }
-}
+  try {
+    const [rows] = await pool.query("SELECT * FROM products");
+    res.json(rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
+
+//Get a product from DB
+export const getProduct = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM products WHERE id = ?", [
+      req.params.id,
+    ]);
+
+    if (rows.length <= 0)
+      return res.status(404).json({
+        message: "product not found",
+      });
+      
+    res.json(rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
--- api-nodejs/src/routes/products.routes.js
+++ api-nodejs/src/routes/products.routes.js
@@ -1,14 +1,14 @@
 //Import router from express
-import {Router} from "express";
+import { Router } from "express";
 //Import products controller
-import { getProducts } from "../controllers/products.controller.js";
+import { getProducts, getProduct } from "../controllers/products.controller.js";
 
 //Create the router
 const router = Router();
 
 //Create http methods connection
-router.get("/products", getProducts); 
-
+router.get("/products", getProducts)
+  .get("/products/:id", getProduct);
 
 //Export the router
 export default router;
